5003011c0a964584a1b2e26be0c9087482c90e46
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-02-04 18:35:09 +0000

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 const config = {
     db: {
-        /* don't expose password or any sensitive info, done only for demo */
         host: "mysql",
         user: "api_user",
+        // password not secure, must be changed for prod 
         password: "password",
         database: "uodLibraryOccupancy",
         port: 3306,
