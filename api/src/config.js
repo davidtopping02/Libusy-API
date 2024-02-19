2ca05c94b019ad713b268d4dec8b270aee444f81
davidtopping02 <2412905@dundee.ac.uk>
2024-02-19 10:55:59 +0000

    project restructure

---
+++ api/src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+        host: "10.8.0.1",
+        user: "expressApi",
+        password: "9$Jp#Ku@5zYv",
+        database: "uodLibraryOccupancy",
+        port: 3306,
+        connectTimeout: 60000
+    },
+    listPerPage: 10,
+};
+module.exports = config;
