8b00a0edee7e3b46438b03d8f164c12b2ece8a14
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-02-20 13:11:09 +0000

    addede in the api functionality

--- total-library-occupancy/main.py
+++ total-library-occupancy/main.py
@@ -10,18 +10,20 @@ def main():
     # lib gates API credentials and URL
     username = 'dtopping'
     password = 'UO33mUx3zEKb%S8b'
-    api_url = 'https://your-library-occupancy-api.com'
 
+    api_url = 'http://10.8.0.1:3000'
     api = LibraryGateAPI(username, password, api_url)
 
-    try:
-        api.run_periodically(interval_minutes=20)
-    except KeyboardInterrupt:
-        # TODO: log the keyboard interupt
-        pass
-    except Exception as e:
-        # TODO: log the error
-        pass
+    api.run_periodically(interval_minutes=20)
+
+    # try:
+    #     api.run_periodically(interval_minutes=20)
+    # except KeyboardInterrupt:
+    #     # TODO: log the keyboard interupt
+    #     pass
+    # except Exception as e:
+    #     # TODO: log the error
+    #     pass
 
 
 if __name__ == "__main__":
