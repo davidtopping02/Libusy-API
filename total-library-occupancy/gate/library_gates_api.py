8ab1381f0ac436dd7b9187e89eb88b1413d91efd
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-02-21 15:29:50 +0000

    fixed logging bug

--- total-library-occupancy/gate/library_gates_api.py
+++ total-library-occupancy/gate/library_gates_api.py
@@ -1,69 +1,48 @@
-import sys
-import requests
-import time
-from datetime import datetime, timedelta
-import os
 import logging
+import sys
+from datetime import datetime, timedelta
+import requests
 
+# Assuming LibraryOccupancyAPI is properly defined in another module
 from api.library_occupancy_api import LibraryOccupancyAPI
 
 
 class LibraryGateAPI:
+    """
+    A class to interact with the Library Gate API and manage occupancy data.
+    """
+
     def __init__(self, username, password, api_url):
         self.username = username
         self.password = password
         self.base_url = 'https://librarygateapi.dundee.ac.uk/librarygatedataapi/api/data'
         self.occupancy_api = LibraryOccupancyAPI(api_url)
 
-    def configure_logging(self):
-        # create log directory if it doesn't exist
-        log_directory = os.path.join(
-            os.getcwd(), "total-library-occupancy", "logs")
-
-        if not os.path.exists(log_directory):
-            os.makedirs(log_directory)
-
-        # generate a log file name with the current date
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        log_file = f'{log_directory}/{current_date}.log'
-
-        # configure logging with the log file and format
-        logging.basicConfig(level=logging.INFO, filename=log_file,
-                            filemode='a', format='%(levelname)s:[%(asctime)s]:%(name)s: %(message)s')
-
-        # Add a StreamHandler to log to stdout as well
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter(
-            '%(levelname)s:[%(asctime)s]:%(name)s: %(message)s')
-        console_handler.setFormatter(formatter)
-        logging.getLogger().addHandler(console_handler)
-
-    def fetch_data(self, interval_minutes):
-        # calculate the time range for the past 20 minutes
+    def fetch_gate_data(self, interval_minutes):
         end_time = datetime.now()
         start_time = end_time - timedelta(minutes=interval_minutes)
 
-        # format the time strings
+        # Offset by 1 minute behind
+        start_time -= timedelta(minutes=1)
+        end_time -= timedelta(minutes=1)
+
         start_time_str = start_time.strftime('%Y-%m-%dT%H:%M:%S.000')
         end_time_str = end_time.strftime('%Y-%m-%dT%H:%M:%S.000')
 
-        # make the API request to the library gates
         auth = (self.username, self.password)
         params = {'from': start_time_str, 'to': end_time_str}
         response = requests.get(self.base_url, auth=auth, params=params)
 
-        # check if the request was successful
         if response.status_code == 200:
-            logging.info("Data fetched successfully.")
+            logging.info("succesfully fetched library gate data from the API")
             return response.json()
         else:
             logging.error(
-                f"Failed to fetch data. Status code: {response.status_code}")
+                f"failed fetched library gate data from the API: {response.status_code}")
             return None
 
     def parse_response(self, data):
-        # checks for valid entry and exits, then calculates the net difference
+        logging.info("parsing library gate data")
         in_count = sum(1 for entry in data if entry.get(
             'EventType') == 'Valid_Access' and 'IN' in entry.get('SourceEntityDescription'))
         out_count = sum(1 for entry in data if entry.get(
@@ -71,42 +50,7 @@ class LibraryGateAPI:
         return in_count - out_count
 
     def update_total_occupancy(self, net_change):
-        # updates the total occupancy based on the newly calculated net difference
         current_occupancy = self.occupancy_api.get_total_occupancy()
         new_occupancy = current_occupancy + net_change
         self.occupancy_api.update_total_occupancy(new_occupancy)
         logging.info(f"Total occupancy updated. Net change: {net_change}")
-
-    def run_periodically(self, interval_minutes):
-        last_executed_calibration_logic = None
-        time.sleep(10)
-
-        while True:
-            current_time = datetime.now()
-            current_date = current_time.date()
-            # check if a new day since the last calibration execution
-            if last_executed_calibration_logic is None or last_executed_calibration_logic < current_date:
-                self.configure_logging()
-
-                if 4 <= current_time.hour < 5:
-                    self.calibration_logic()
-                    last_executed_calibration_logic = current_date
-                    logging.info("calibration logic executed.")
-
-            # Fetch data from the library gates
-            data = self.fetch_data(interval_minutes)
-
-            if data:
-                net_change = self.parse_response(data)
-                self.update_total_occupancy(net_change)
-            else:
-                logging.error(
-                    f"data was empty from the library gates")
-
-            logging.info(
-                f"Waiting for {interval_minutes} minutes before the next request...")
-            time.sleep(interval_minutes * 60)
-
-    def calibration_logic(self):
-        self.occupancy_api.update_total_occupancy(
-            self.occupancy_api.get_total_base())
--- total-library-occupancy/main.py
+++ total-library-occupancy/main.py
@@ -1,19 +1,64 @@
+import os
+import logging
 import time
+from datetime import datetime, timedelta
 from gate.library_gates_api import LibraryGateAPI
+
+
+class TotalOccupancyManager:
+    def __init__(self, api_url, username, password, interval_minutes):
+        self.api = LibraryGateAPI(username, password, api_url)
+        self.interval_minutes = interval_minutes
+        self.last_calibration_date = None
+
+    def configure_logging(self):
+        current_date = datetime.now().date()
+        log_directory = os.path.join(
+            os.getcwd(), "total-library-occupancy", "logs")
+        if not os.path.exists(log_directory):
+            os.makedirs(log_directory)
+        log_file = os.path.join(log_directory, f'{current_date}.log')
+        logging.basicConfig(level=logging.INFO, filename=log_file,
+                            filemode='a', format='%(levelname)s:[%(asctime)s]:%(name)s: %(message)s')
+
+    def total_occupancy_calibration(self):
+        self.api.update_total_occupancy(self.api.get_total_base())
+        logging.info("total occupancy calibrated")
+
+    def run_periodically(self):
+
+        self.configure_logging()
+
+        while True:
+            current_time = datetime.now()
+            current_date = current_time.date()
+
+            if self.last_calibration_date != current_date and 4 <= current_time.hour < 5:
+                self.configure_logging()
+                self.total_occupancy_calibration()
+                self.last_calibration_date = current_date
+
+            data = self.api.fetch_gate_data(self.interval_minutes)
+
+            if data:
+                net_change = self.api.parse_response(data)
+                self.api.update_total_occupancy(net_change)
+            else:
+                logging.error("Data was empty from the library gates")
+            logging.info(
+                f"Waiting for {self.interval_minutes} minutes before the next request...")
+            time.sleep(self.interval_minutes * 60)
 
 
 def main():
     print("STARTED")
-    # lib gates API credentials and URL
     username = 'dtopping'
     password = 'UO33mUx3zEKb%S8b'
-
-    # api_url = 'http://10.8.0.1:3000'
-    api_url = 'http://uod-lib-occupancy-api:3000'
-
-    api = LibraryGateAPI(username, password, api_url)
-
-    api.run_periodically(interval_minutes=20)
+    api_url = 'http://10.8.0.1:3000'
+    # For Docker use: api_url = 'http://uod-lib-occupancy-api:3000'
+    manager = TotalOccupancyManager(
+        api_url, username, password, interval_minutes=0.5)
+    manager.run_periodically()
 
 
 if __name__ == "__main__":
