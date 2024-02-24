3d409ff44b122598582f30cf84a8888a2467c464
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-02-24 09:56:26 +0000

    improved db handing

--- api/src/services/db.js
+++ api/src/services/db.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql2/promise');
 const config = require('../config');
 
-async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
-    const [results,] = await connection.execute(sql, params);
+// Create connection pool
+const pool = mysql.createPool(config.db);
 
-    return results;
+async function query(sql, params) {
+    const connection = await pool.getConnection(); // Get a connection from the pool
+    try {
+        const [results,] = await connection.execute(sql, params);
+        return results;
+    } finally {
+        connection.release(); // Release the connection back to the pool
+    }
 }
 
 module.exports = {
     query
-}
+};
