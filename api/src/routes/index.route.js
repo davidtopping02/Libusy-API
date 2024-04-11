709bc010f9e14d94d0cdbc45628520c41b5ad70b
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-04-11 12:42:19 +0100

    added swagger documentation

--- api/src/routes/index.route.js
+++ api/src/routes/index.route.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Welcome message
+ *     description: Returns a welcome message for the UoD Library Occupancy API.
+ *     responses:
+ *       200:
+ *         description: A welcome message.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Welcome message.
+ *                   example: "welcome to the UoD Library Occupancy API"
+ */
 router.get('/', async function (req, res, next) {
-    res.json({ message: "welcome to the UoD Library Occupancy API" });
+    res.json({ message: "welcome to the UoD Library Occupancy API! Documentation can be found at /docs" });
 });
 
 module.exports = router;
